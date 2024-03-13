41fa1f2bb743294b3e487eea5231b3aea0e87467
haaslaura <110221401+haaslaura@users.noreply.github.com>
2024-03-13 14:21:39 +0100

    Corrections so that the focus search bar has an outline and not a border

--- scripts/templates/searchBar.js
+++ scripts/templates/searchBar.js
@@ -7,12 +7,12 @@ This JavaScript code is used for apply a style to the searchbar
 
 // Add an event to the input when it is in focus
 document.getElementById("search").addEventListener("focus", function () {
-    document.querySelector(".input-group").style.border = "2px solid rgb(255, 209, 91)";
-    document.querySelector(".input-group").style.borderRadius = "14px";
+	document.querySelector(".input-group").style.outline = "3px solid rgb(255, 209, 91)";
+	document.querySelector(".input-group").style.borderRadius = "14px";
 });
 
 // Add an event to the input when it loses focus
 document.getElementById("search").addEventListener("blur", function () {
-    document.querySelector(".input-group").style.border = "";
-    document.querySelector(".input-group").style.borderRadius = "";
+	document.querySelector(".input-group").style.outline = "";
+	document.querySelector(".input-group").style.borderRadius = "";
 });
